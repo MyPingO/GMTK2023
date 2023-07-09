1aa989380f44dfd25e5195550cffc597045470a7
Nathaniel Alcala <semblancewarden@Gmail.com>
2023-07-09 13:45:10 +0800

    ✨ Made pause mode slightly fancier

--- Assets/Scripts/LevelManagement/PauseManager.cs
+++ Assets/Scripts/LevelManagement/PauseManager.cs
@@ -9,20 +9,28 @@ using UnityEngine.Rendering.Universal;
 
 public class PauseManager : LevelComponent
 {
+    [System.Serializable]
+    public class SpriteLayer
+    {
+        [SerializeField] private Transform spriteGroup;
+        [SerializeField] private float offsetDistance;
+
+        public Transform SpriteGroup => spriteGroup;
+        public float OffsetDistance => offsetDistance;
+    }
+
     [SerializeField] private Page pausePage;
     [SerializeField] private PageController pageController;
     [SerializeField] private CinemachineVirtualCamera pauseCamera;
-    [SerializeField] private VolumeProfile volume;
+
+    [Header("Offset")]
+    [SerializeField] private float offsetDelay = 0.1f;
+    [SerializeField] private float offsetLerpTime = 0.5f;
+    [SerializeField] private SpriteLayer[] spriteLayers;
+    [SerializeField] private Ease easeType = Ease.InOutQuint;
 
     private bool isPaused;
     private DepthOfField dofComponent;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        volume.TryGet(out dofComponent);
-    }
 
     private void OnEnable()
     {
@@ -44,14 +52,33 @@ public class PauseManager : LevelComponent
             level.Settings.Events.OnPaused?.Invoke();
             pageController.OpenPage(pausePage);
             Time.timeScale = 0;
-            dofComponent.focusDistance.value = 2;
+            StartCoroutine(LerpOutLayers());
         }
         else
         {
             level.Settings.Events.OnResumed?.Invoke();
             pageController.CloseTopPage();
             Time.timeScale = 1;
-            dofComponent.focusDistance.value = 10;
+            LerpInLayers();
+        }
+    }
+
+    private IEnumerator LerpOutLayers()
+    {
+        int counter = 0;
+        while (counter < spriteLayers.Length)
+        {
+            yield return new WaitForSecondsRealtime(offsetDelay);
+            spriteLayers[counter].SpriteGroup.DOMoveZ(spriteLayers[counter].OffsetDistance, offsetLerpTime).SetEase(easeType);
+            counter++;
+        }
+    }
+
+    private void LerpInLayers()
+    {
+        for (int i = 0; i < spriteLayers.Length; i++)
+        {
+            spriteLayers[i].SpriteGroup.DOMoveZ(0, offsetLerpTime).SetEase(Ease.InOutSine);
         }
     }
 }
